47486f1b66a34cc67a86fde992f35c68f797cf0f
Andrew Jones <ascjones@gmail.com>
2020-12-18 10:09:57 +0000

    WIP v13

---
+++ frame-metadata/src/v13.rs
@@ -0,0 +1,257 @@
+// This file is part of Substrate.
+
+// Copyright (C) 2018-2020 Parity Technologies (UK) Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use scale_info::{
+    form::{CompactForm, Form, FormString, MetaForm},
+    meta_type, IntoCompact, Registry, RegistryReadOnly, TypeInfo,
+};
+
+impl From<RuntimeMetadataLastVersion<MetaForm>> for RuntimeMetadataPrefixed {
+    fn from(metadata: RuntimeMetadataLastVersion<MetaForm>) -> RuntimeMetadataPrefixed {
+        let mut registry = Registry::new();
+        let metadata = metadata.into_compact(&mut registry);
+        RuntimeMetadataPrefixed {
+            prefix: super::META_RESERVED,
+            types: registry.into(),
+            metadata: RuntimeMetadata::V12(metadata),
+        }
+    }
+}
+
+/// The metadata of a runtime.
+#[derive(Clone, PartialEq, Eq, Encode, Debug)]
+#[cfg_attr(feature = "std", derive(Decode))]
+pub struct RuntimeMetadataV13<T: Form = MetaForm> {
+    pub types: RegistryReadOnly<S>,
+    /// Metadata of all the modules.
+    pub modules: Vec<ModuleMetadata<T>>,
+    // /// Metadata of the extrinsic.
+    // pub extrinsic: ExtrinsicMetadata<F>,
+}
+
+impl RuntimeMetadataV13 {
+    pub fn new(modules: Vec<ModuleMetadata>) -> Self {
+
+    }
+}
+
+impl IntoCompact for RuntimeMetadataV13 {
+    type Output = RuntimeMetadataV13<CompactForm>;
+
+    fn into_compact(self, registry: &mut Registry) -> Self::Output {
+        RuntimeMetadataV13 {
+            modules: registry.map_into_compact(self.modules),
+            // extrinsic: self.extrinsic.into_compact(registry),
+        }
+    }
+}
+
+/// Metadata of the extrinsic used by the runtime.
+#[derive(Clone, PartialEq, Eq, Encode, Debug)]
+#[cfg_attr(feature = "std", derive(Decode))]
+pub struct ExtrinsicMetadata<T: Form = MetaForm> {
+    /// Extrinsic version.
+    pub version: u8,
+    /// The signed extensions in the order they appear in the extrinsic.
+    pub signed_extensions: Vec<T::Type>,
+}
+
+impl IntoCompact for ExtrinsicMetadata {
+    type Output = ExtrinsicMetadata<CompactForm>;
+
+    fn into_compact(self, registry: &mut Registry) -> Self::Output {
+        ExtrinsicMetadata {
+            version: self.version,
+            signed_extensions: registry.register_types(self.signed_extensions),
+        }
+    }
+}
+
+/// All metadata about an runtime module.
+#[derive(Clone, PartialEq, Eq, Encode, Debug)]
+#[cfg_attr(feature = "std", derive(Decode))]
+pub struct ModuleMetadata<T: Form = MetaForm> {
+    pub name: T::String,
+    // pub storage: Option<DecodeDifferent<FnEncode<StorageMetadata>, StorageMetadata>>,
+    pub calls: Option<Vec<FunctionMetadata<T>>>,
+    pub event: Option<Vec<EventMetadata<T>>>,
+    // pub constants: DFnA<ModuleConstantMetadata>,
+    // pub errors: DFnA<ErrorMetadata>,
+}
+
+impl IntoCompact for ModuleMetadata {
+    type Output = ModuleMetadata<CompactForm>;
+
+    fn into_compact(self, registry: &mut Registry) -> Self::Output {
+        ModuleMetadata {
+            name: self.name.into_compact(registry),
+            calls: self.calls.map(|calls| registry.map_into_compact(calls)),
+            event: self.event.map(|event| registry.map_into_compact(event)),
+        }
+    }
+}
+
+/// All the metadata about a function.
+#[derive(Clone, PartialEq, Eq, Encode, Debug)]
+#[cfg_attr(feature = "std", derive(Decode))]
+pub struct FunctionMetadata<T: Form = MetaForm> {
+    pub name: T::String,
+    pub arguments: Vec<FunctionArgumentMetadata<T>>,
+    pub documentation: Vec<T::String>,
+}
+
+impl IntoCompact for FunctionMetadata {
+    type Output = FunctionMetadata<CompactForm>;
+
+    fn into_compact(self, registry: &mut Registry) -> Self::Output {
+        FunctionMetadata {
+            name: self.name.into_compact(registry),
+            arguments: registry.map_into_compact(self.arguments),
+            documentation: registry.map_into_compact(self.documentation),
+        }
+    }
+}
+
+/// All the metadata about a function argument.
+#[derive(Clone, PartialEq, Eq, Encode, Debug)]
+#[cfg_attr(feature = "std", derive(Decode))]
+pub struct FunctionArgumentMetadata<T: Form = MetaForm> {
+    pub name: T::String,
+    pub ty: T::Type,
+    pub is_compact: bool,
+}
+
+impl IntoCompact for FunctionArgumentMetadata {
+    type Output = FunctionArgumentMetadata<CompactForm>;
+
+    fn into_compact(self, registry: &mut Registry) -> Self::Output {
+        FunctionArgumentMetadata {
+            name: self.name.into_compact(registry),
+            ty: registry.register_type(&self.ty),
+            is_compact: self.is_compact,
+        }
+    }
+}
+
+/// All the metadata about an outer event.
+#[derive(Clone, PartialEq, Eq, Encode, Debug)]
+#[cfg_attr(feature = "std", derive(Decode))]
+pub struct OuterEventMetadata<T: Form = MetaForm> {
+    pub name: T::String,
+    pub events: Vec<ModuleEventMetadata<T>>,
+}
+
+impl IntoCompact for OuterEventMetadata {
+    type Output = OuterEventMetadata<CompactForm>;
+
+    fn into_compact(self, registry: &mut Registry) -> Self::Output {
+        OuterEventMetadata {
+            name: self.name.into_compact(registry),
+            events: registry.map_into_compact(self.events),
+        }
+    }
+}
+
+/// Metadata about a module event.
+#[derive(Clone, PartialEq, Eq, Encode, Debug)]
+#[cfg_attr(feature = "std", derive(Decode))]
+pub struct ModuleEventMetadata<T: Form = MetaForm> {
+    pub name: T::String,
+    pub events: Vec<EventMetadata<T>>,
+}
+
+impl IntoCompact for ModuleEventMetadata {
+    type Output = ModuleEventMetadata<CompactForm>;
+
+    fn into_compact(self, registry: &mut Registry) -> Self::Output {
+        ModuleEventMetadata {
+            name: self.name.into_compact(registry),
+            events: registry.map_into_compact(self.events),
+        }
+    }
+}
+
+/// All the metadata about an event.
+#[derive(Clone, PartialEq, Eq, Encode, Debug)]
+#[cfg_attr(feature = "std", derive(Decode))]
+pub struct EventMetadata<T: Form = MetaForm> {
+    pub name: T::String,
+    pub arguments: Vec<TypeSpec<T>>,
+    pub documentation: Vec<T::String>,
+}
+
+impl IntoCompact for EventMetadata {
+    type Output = EventMetadata<CompactForm>;
+
+    fn into_compact(self, registry: &mut Registry) -> Self::Output {
+        EventMetadata {
+            name: self.name.into_compact(registry),
+            arguments: registry.map_into_compact(self.arguments),
+            documentation: registry.map_into_compact(self.documentation),
+        }
+    }
+}
+
+/// A type specification.
+///
+/// This contains the actual type as well as an optional compile-time
+/// known displayed representation of the type. This is useful for cases
+/// where the type is used through a type alias in order to provide
+/// information about the alias name.
+///
+/// # Examples
+///
+/// Consider the following Rust function:
+/// ```no_compile
+/// fn is_sorted(input: &[i32], pred: Predicate) -> bool;
+/// ```
+/// In this above example `input` would have no displayable name,
+/// `pred`'s display name is `Predicate` and the display name of
+/// the return type is simply `bool`. Note that `Predicate` could
+/// simply be a type alias to `fn(i32, i32) -> Ordering`.
+#[derive(Clone, PartialEq, Eq, Encode, Debug)]
+#[cfg_attr(feature = "std", derive(Decode))]
+pub struct TypeSpec<T: Form = MetaForm> {
+    /// The actual type.
+    pub ty: T::Type,
+    /// The compile-time known displayed representation of the type.
+    pub name: T::String,
+}
+
+impl IntoCompact for TypeSpec {
+    type Output = TypeSpec<CompactForm>;
+
+    fn into_compact(self, registry: &mut Registry) -> Self::Output {
+        TypeSpec {
+            ty: registry.register_type(&self.ty),
+            name: self.name.into_compact(registry),
+        }
+    }
+}
+
+impl TypeSpec {
+    /// Creates a new type specification without a display name.
+    pub fn new<T>(name: &'static str) -> Self
+        where
+            T: TypeInfo + 'static,
+    {
+        Self {
+            ty: meta_type::<T>(),
+            name,
+        }
+    }
+}
