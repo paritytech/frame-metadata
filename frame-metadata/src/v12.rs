4634602d0baf76be9a6266feffd8ea88035b53bd
Andrew Jones <ascjones@gmail.com>
2021-01-05 17:02:39 +0000

    Fmt

--- frame-metadata/src/v12.rs
+++ frame-metadata/src/v12.rs
@@ -32,8 +32,8 @@ cfg_if::cfg_if! {
 		use alloc::vec::Vec;
 
 		/// On `no_std` we do not support `Decode` and thus `StringBuf` is just `&'static str`.
-        /// So, if someone tries to decode this stuff on `no_std`, they will get a compilation error.
-        type StringBuf = &'static str;
+		/// So, if someone tries to decode this stuff on `no_std`, they will get a compilation error.
+		type StringBuf = &'static str;
 	}
 }
 
@@ -51,90 +51,90 @@ type StringBuf = &'static str;
 /// For example a `&'static [ &'static str ]` can be decoded to a `Vec<String>`.
 #[derive(Clone)]
 pub enum DecodeDifferent<B, O>
-    where
-        B: 'static,
-        O: 'static,
+where
+	B: 'static,
+	O: 'static,
 {
-    Encode(B),
-    Decoded(O),
+	Encode(B),
+	Decoded(O),
 }
 
 impl<B, O> Encode for DecodeDifferent<B, O>
-    where
-        B: Encode + 'static,
-        O: Encode + 'static,
+where
+	B: Encode + 'static,
+	O: Encode + 'static,
 {
-    fn encode_to<W: Output>(&self, dest: &mut W) {
-        match self {
-            DecodeDifferent::Encode(b) => b.encode_to(dest),
-            DecodeDifferent::Decoded(o) => o.encode_to(dest),
-        }
-    }
+	fn encode_to<W: Output>(&self, dest: &mut W) {
+		match self {
+			DecodeDifferent::Encode(b) => b.encode_to(dest),
+			DecodeDifferent::Decoded(o) => o.encode_to(dest),
+		}
+	}
 }
 
 impl<B, O> codec::EncodeLike for DecodeDifferent<B, O>
-    where
-        B: Encode + 'static,
-        O: Encode + 'static,
+where
+	B: Encode + 'static,
+	O: Encode + 'static,
 {
 }
 
 #[cfg(feature = "std")]
 impl<B, O> Decode for DecodeDifferent<B, O>
-    where
-        B: 'static,
-        O: Decode + 'static,
+where
+	B: 'static,
+	O: Decode + 'static,
 {
-    fn decode<I: Input>(input: &mut I) -> Result<Self, Error> {
-        <O>::decode(input).map(|val| DecodeDifferent::Decoded(val))
-    }
+	fn decode<I: Input>(input: &mut I) -> Result<Self, Error> {
+		<O>::decode(input).map(|val| DecodeDifferent::Decoded(val))
+	}
 }
 
 impl<B, O> PartialEq for DecodeDifferent<B, O>
-    where
-        B: Encode + Eq + PartialEq + 'static,
-        O: Encode + Eq + PartialEq + 'static,
+where
+	B: Encode + Eq + PartialEq + 'static,
+	O: Encode + Eq + PartialEq + 'static,
 {
-    fn eq(&self, other: &Self) -> bool {
-        self.encode() == other.encode()
-    }
+	fn eq(&self, other: &Self) -> bool {
+		self.encode() == other.encode()
+	}
 }
 
 impl<B, O> Eq for DecodeDifferent<B, O>
-    where
-        B: Encode + Eq + PartialEq + 'static,
-        O: Encode + Eq + PartialEq + 'static,
+where
+	B: Encode + Eq + PartialEq + 'static,
+	O: Encode + Eq + PartialEq + 'static,
 {
 }
 
 impl<B, O> core::fmt::Debug for DecodeDifferent<B, O>
-    where
-        B: core::fmt::Debug + Eq + 'static,
-        O: core::fmt::Debug + Eq + 'static,
+where
+	B: core::fmt::Debug + Eq + 'static,
+	O: core::fmt::Debug + Eq + 'static,
 {
-    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
-        match self {
-            DecodeDifferent::Encode(b) => b.fmt(f),
-            DecodeDifferent::Decoded(o) => o.fmt(f),
-        }
-    }
+	fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+		match self {
+			DecodeDifferent::Encode(b) => b.fmt(f),
+			DecodeDifferent::Decoded(o) => o.fmt(f),
+		}
+	}
 }
 
 #[cfg(feature = "std")]
 impl<B, O> serde::Serialize for DecodeDifferent<B, O>
-    where
-        B: serde::Serialize + 'static,
-        O: serde::Serialize + 'static,
+where
+	B: serde::Serialize + 'static,
+	O: serde::Serialize + 'static,
 {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-        where
-            S: serde::Serializer,
-    {
-        match self {
-            DecodeDifferent::Encode(b) => b.serialize(serializer),
-            DecodeDifferent::Decoded(o) => o.serialize(serializer),
-        }
-    }
+	fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+	where
+		S: serde::Serializer,
+	{
+		match self {
+			DecodeDifferent::Encode(b) => b.serialize(serializer),
+			DecodeDifferent::Decoded(o) => o.serialize(serializer),
+		}
+	}
 }
 
 pub type DecodeDifferentArray<B, O = B> = DecodeDifferent<&'static [B], Vec<O>>;
@@ -145,118 +145,118 @@ type DecodeDifferentStr = DecodeDifferent<&'static str, StringBuf>;
 #[derive(Clone, PartialEq, Eq, Encode)]
 #[cfg_attr(feature = "std", derive(Decode, Serialize, Debug))]
 pub struct FunctionMetadata {
-    pub name: DecodeDifferentStr,
-    pub arguments: DecodeDifferentArray<FunctionArgumentMetadata>,
-    pub documentation: DecodeDifferentArray<&'static str, StringBuf>,
+	pub name: DecodeDifferentStr,
+	pub arguments: DecodeDifferentArray<FunctionArgumentMetadata>,
+	pub documentation: DecodeDifferentArray<&'static str, StringBuf>,
 }
 
 /// All the metadata about a function argument.
 #[derive(Clone, PartialEq, Eq, Encode)]
 #[cfg_attr(feature = "std", derive(Decode, Serialize, Debug))]
 pub struct FunctionArgumentMetadata {
-    pub name: DecodeDifferentStr,
-    pub ty: DecodeDifferentStr,
+	pub name: DecodeDifferentStr,
+	pub ty: DecodeDifferentStr,
 }
 
 /// Newtype wrapper for support encoding functions (actual the result of the function).
 #[derive(Clone, Eq)]
 pub struct FnEncode<E>(pub fn() -> E)
-    where
-        E: Encode + 'static;
+where
+	E: Encode + 'static;
 
 impl<E: Encode> Encode for FnEncode<E> {
-    fn encode_to<W: Output>(&self, dest: &mut W) {
-        self.0().encode_to(dest);
-    }
+	fn encode_to<W: Output>(&self, dest: &mut W) {
+		self.0().encode_to(dest);
+	}
 }
 
 impl<E: Encode> codec::EncodeLike for FnEncode<E> {}
 
 impl<E: Encode + PartialEq> PartialEq for FnEncode<E> {
-    fn eq(&self, other: &Self) -> bool {
-        self.0().eq(&other.0())
-    }
+	fn eq(&self, other: &Self) -> bool {
+		self.0().eq(&other.0())
+	}
 }
 
 impl<E: Encode + core::fmt::Debug> core::fmt::Debug for FnEncode<E> {
-    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
-        self.0().fmt(f)
-    }
+	fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+		self.0().fmt(f)
+	}
 }
 
 #[cfg(feature = "std")]
 impl<E: Encode + serde::Serialize> serde::Serialize for FnEncode<E> {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-        where
-            S: serde::Serializer,
-    {
-        self.0().serialize(serializer)
-    }
+	fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+	where
+		S: serde::Serializer,
+	{
+		self.0().serialize(serializer)
+	}
 }
 
 /// All the metadata about an outer event.
 #[derive(Clone, PartialEq, Eq, Encode)]
 #[cfg_attr(feature = "std", derive(Decode, Serialize, Debug))]
 pub struct OuterEventMetadata {
-    pub name: DecodeDifferentStr,
-    pub events: DecodeDifferentArray<
-        (&'static str, FnEncode<&'static [EventMetadata]>),
-        (StringBuf, Vec<EventMetadata>),
-    >,
+	pub name: DecodeDifferentStr,
+	pub events: DecodeDifferentArray<
+		(&'static str, FnEncode<&'static [EventMetadata]>),
+		(StringBuf, Vec<EventMetadata>),
+	>,
 }
 
 /// All the metadata about an event.
 #[derive(Clone, PartialEq, Eq, Encode)]
 #[cfg_attr(feature = "std", derive(Decode, Serialize, Debug))]
 pub struct EventMetadata {
-    pub name: DecodeDifferentStr,
-    pub arguments: DecodeDifferentArray<&'static str, StringBuf>,
-    pub documentation: DecodeDifferentArray<&'static str, StringBuf>,
+	pub name: DecodeDifferentStr,
+	pub arguments: DecodeDifferentArray<&'static str, StringBuf>,
+	pub documentation: DecodeDifferentArray<&'static str, StringBuf>,
 }
 
 /// All the metadata about one storage entry.
 #[derive(Clone, PartialEq, Eq, Encode)]
 #[cfg_attr(feature = "std", derive(Decode, Serialize, Debug))]
 pub struct StorageEntryMetadata {
-    pub name: DecodeDifferentStr,
-    pub modifier: StorageEntryModifier,
-    pub ty: StorageEntryType,
-    pub default: ByteGetter,
-    pub documentation: DecodeDifferentArray<&'static str, StringBuf>,
+	pub name: DecodeDifferentStr,
+	pub modifier: StorageEntryModifier,
+	pub ty: StorageEntryType,
+	pub default: ByteGetter,
+	pub documentation: DecodeDifferentArray<&'static str, StringBuf>,
 }
 
 /// All the metadata about one module constant.
 #[derive(Clone, PartialEq, Eq, Encode)]
 #[cfg_attr(feature = "std", derive(Decode, Serialize, Debug))]
 pub struct ModuleConstantMetadata {
-    pub name: DecodeDifferentStr,
-    pub ty: DecodeDifferentStr,
-    pub value: ByteGetter,
-    pub documentation: DecodeDifferentArray<&'static str, StringBuf>,
+	pub name: DecodeDifferentStr,
+	pub ty: DecodeDifferentStr,
+	pub value: ByteGetter,
+	pub documentation: DecodeDifferentArray<&'static str, StringBuf>,
 }
 
 /// All the metadata about a module error.
 #[derive(Clone, PartialEq, Eq, Encode)]
 #[cfg_attr(feature = "std", derive(Decode, Serialize, Debug))]
 pub struct ErrorMetadata {
-    pub name: DecodeDifferentStr,
-    pub documentation: DecodeDifferentArray<&'static str, StringBuf>,
+	pub name: DecodeDifferentStr,
+	pub documentation: DecodeDifferentArray<&'static str, StringBuf>,
 }
 
 /// All the metadata about errors in a module.
 pub trait ModuleErrorMetadata {
-    fn metadata() -> &'static [ErrorMetadata];
+	fn metadata() -> &'static [ErrorMetadata];
 }
 
 impl ModuleErrorMetadata for &'static str {
-    fn metadata() -> &'static [ErrorMetadata] {
-        &[]
-    }
+	fn metadata() -> &'static [ErrorMetadata] {
+		&[]
+	}
 }
 
 /// A technical trait to store lazy initiated vec value as static dyn pointer.
 pub trait DefaultByte: Send + Sync {
-    fn default_byte(&self) -> Vec<u8>;
+	fn default_byte(&self) -> Vec<u8>;
 }
 
 /// Wrapper over dyn pointer for accessing a cached once byte value.
@@ -267,88 +267,88 @@ pub struct DefaultByteGetter(pub &'static dyn DefaultByte);
 pub type ByteGetter = DecodeDifferent<DefaultByteGetter, Vec<u8>>;
 
 impl Encode for DefaultByteGetter {
-    fn encode_to<W: Output>(&self, dest: &mut W) {
-        self.0.default_byte().encode_to(dest)
-    }
+	fn encode_to<W: Output>(&self, dest: &mut W) {
+		self.0.default_byte().encode_to(dest)
+	}
 }
 
 impl codec::EncodeLike for DefaultByteGetter {}
 
 impl PartialEq<DefaultByteGetter> for DefaultByteGetter {
-    fn eq(&self, other: &DefaultByteGetter) -> bool {
-        let left = self.0.default_byte();
-        let right = other.0.default_byte();
-        left.eq(&right)
-    }
+	fn eq(&self, other: &DefaultByteGetter) -> bool {
+		let left = self.0.default_byte();
+		let right = other.0.default_byte();
+		left.eq(&right)
+	}
 }
 
 impl Eq for DefaultByteGetter {}
 
 #[cfg(feature = "std")]
 impl serde::Serialize for DefaultByteGetter {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-        where
-            S: serde::Serializer,
-    {
-        self.0.default_byte().serialize(serializer)
-    }
+	fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+	where
+		S: serde::Serializer,
+	{
+		self.0.default_byte().serialize(serializer)
+	}
 }
 
 impl core::fmt::Debug for DefaultByteGetter {
-    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
-        self.0.default_byte().fmt(f)
-    }
+	fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+		self.0.default_byte().fmt(f)
+	}
 }
 
 /// Hasher used by storage maps
 #[derive(Clone, PartialEq, Eq, Encode)]
 #[cfg_attr(feature = "std", derive(Decode, Serialize, Debug))]
 pub enum StorageHasher {
-    Blake2_128,
-    Blake2_256,
-    Blake2_128Concat,
-    Twox128,
-    Twox256,
-    Twox64Concat,
-    Identity,
+	Blake2_128,
+	Blake2_256,
+	Blake2_128Concat,
+	Twox128,
+	Twox256,
+	Twox64Concat,
+	Identity,
 }
 
 /// A storage entry type.
 #[derive(Clone, PartialEq, Eq, Encode)]
 #[cfg_attr(feature = "std", derive(Decode, Serialize, Debug))]
 pub enum StorageEntryType {
-    Plain(DecodeDifferentStr),
-    Map {
-        hasher: StorageHasher,
-        key: DecodeDifferentStr,
-        value: DecodeDifferentStr,
-        // is_linked flag previously, unused now to keep backwards compat
-        unused: bool,
-    },
-    DoubleMap {
-        hasher: StorageHasher,
-        key1: DecodeDifferentStr,
-        key2: DecodeDifferentStr,
-        value: DecodeDifferentStr,
-        key2_hasher: StorageHasher,
-    },
+	Plain(DecodeDifferentStr),
+	Map {
+		hasher: StorageHasher,
+		key: DecodeDifferentStr,
+		value: DecodeDifferentStr,
+		// is_linked flag previously, unused now to keep backwards compat
+		unused: bool,
+	},
+	DoubleMap {
+		hasher: StorageHasher,
+		key1: DecodeDifferentStr,
+		key2: DecodeDifferentStr,
+		value: DecodeDifferentStr,
+		key2_hasher: StorageHasher,
+	},
 }
 
 /// A storage entry modifier.
 #[derive(Clone, PartialEq, Eq, Encode)]
 #[cfg_attr(feature = "std", derive(Decode, Serialize, Debug))]
 pub enum StorageEntryModifier {
-    Optional,
-    Default,
+	Optional,
+	Default,
 }
 
 /// All metadata of the storage.
 #[derive(Clone, PartialEq, Eq, Encode)]
 #[cfg_attr(feature = "std", derive(Decode, Serialize, Debug))]
 pub struct StorageMetadata {
-    /// The common prefix used by all storage entries.
-    pub prefix: DecodeDifferent<&'static str, StringBuf>,
-    pub entries: DecodeDifferent<&'static [StorageEntryMetadata], Vec<StorageEntryMetadata>>,
+	/// The common prefix used by all storage entries.
+	pub prefix: DecodeDifferent<&'static str, StringBuf>,
+	pub entries: DecodeDifferent<&'static [StorageEntryMetadata], Vec<StorageEntryMetadata>>,
 }
 
 /// Metadata prefixed by a u32 for reserved usage
@@ -360,20 +360,20 @@ pub struct RuntimeMetadataPrefixed(pub u32, pub super::RuntimeMetadata);
 #[derive(Eq, Encode, PartialEq)]
 #[cfg_attr(feature = "std", derive(Decode, Serialize, Debug))]
 pub struct ExtrinsicMetadata {
-    /// Extrinsic version.
-    pub version: u8,
-    /// The signed extensions in the order they appear in the extrinsic.
-    pub signed_extensions: Vec<DecodeDifferentStr>,
+	/// Extrinsic version.
+	pub version: u8,
+	/// The signed extensions in the order they appear in the extrinsic.
+	pub signed_extensions: Vec<DecodeDifferentStr>,
 }
 
 /// The metadata of a runtime.
 #[derive(Eq, Encode, PartialEq, Debug)]
 #[cfg_attr(feature = "std", derive(Decode, Serialize))]
 pub struct RuntimeMetadataV12 {
-    /// Metadata of all the modules.
-    pub modules: DecodeDifferentArray<ModuleMetadata>,
-    /// Metadata of the extrinsic.
-    pub extrinsic: ExtrinsicMetadata,
+	/// Metadata of all the modules.
+	pub modules: DecodeDifferentArray<ModuleMetadata>,
+	/// Metadata of the extrinsic.
+	pub extrinsic: ExtrinsicMetadata,
 }
 
 /// The latest version of the metadata.
@@ -383,22 +383,22 @@ pub type RuntimeMetadataLastVersion = RuntimeMetadataV12;
 #[derive(Clone, PartialEq, Eq, Encode)]
 #[cfg_attr(feature = "std", derive(Decode, Serialize, Debug))]
 pub struct ModuleMetadata {
-    pub name: DecodeDifferentStr,
-    pub storage: Option<DecodeDifferent<FnEncode<StorageMetadata>, StorageMetadata>>,
-    pub calls: ODFnA<FunctionMetadata>,
-    pub event: ODFnA<EventMetadata>,
-    pub constants: DFnA<ModuleConstantMetadata>,
-    pub errors: DFnA<ErrorMetadata>,
-    /// Define the index of the module, this index will be used for the encoding of module event,
-    /// call and origin variants.
-    pub index: u8,
+	pub name: DecodeDifferentStr,
+	pub storage: Option<DecodeDifferent<FnEncode<StorageMetadata>, StorageMetadata>>,
+	pub calls: ODFnA<FunctionMetadata>,
+	pub event: ODFnA<EventMetadata>,
+	pub constants: DFnA<ModuleConstantMetadata>,
+	pub errors: DFnA<ErrorMetadata>,
+	/// Define the index of the module, this index will be used for the encoding of module event,
+	/// call and origin variants.
+	pub index: u8,
 }
 
 type ODFnA<T> = Option<DFnA<T>>;
 type DFnA<T> = DecodeDifferent<FnEncode<&'static [T]>, Vec<T>>;
 
 impl Into<Vec<u8>> for RuntimeMetadataPrefixed {
-    fn into(self) -> Vec<u8> {
-        self.encode()
-    }
+	fn into(self) -> Vec<u8> {
+		self.encode()
+	}
 }
--- frame-metadata/src/v13.rs
+++ frame-metadata/src/v13.rs
@@ -16,18 +16,18 @@
 // limitations under the License.
 
 cfg_if::cfg_if! {
-    if #[cfg(feature = "std")] {
-        use codec::{Decode, Error, Input};
-        use serde::Serialize;
-    }
+	if #[cfg(feature = "std")] {
+		use codec::{Decode, Error, Input};
+		use serde::Serialize;
+	}
 }
 
-use scale_info::{
-    form::{PortableForm, Form, FormString, MetaForm},
-    meta_type, IntoPortable, Registry, PortableRegistry, TypeInfo,
-};
-use scale_info::prelude::vec::Vec;
 use codec::Encode;
+use scale_info::prelude::vec::Vec;
+use scale_info::{
+	form::{Form, FormString, MetaForm, PortableForm},
+	meta_type, IntoPortable, PortableRegistry, Registry, TypeInfo,
+};
 
 /// Current prefix of metadata
 pub const META_RESERVED: u32 = 0x6174656d; // 'meta' warn endianness
@@ -40,9 +40,9 @@ pub struct RuntimeMetadataPrefixed(pub u32, pub super::RuntimeMetadata);
 pub type RuntimeMetadataLastVersion = RuntimeMetadataV13;
 
 impl From<RuntimeMetadataLastVersion> for RuntimeMetadataPrefixed {
-    fn from(metadata: RuntimeMetadataLastVersion) -> RuntimeMetadataPrefixed {
-        RuntimeMetadataPrefixed(META_RESERVED, super::RuntimeMetadata::V13(metadata))
-    }
+	fn from(metadata: RuntimeMetadataLastVersion) -> RuntimeMetadataPrefixed {
+		RuntimeMetadataPrefixed(META_RESERVED, super::RuntimeMetadata::V13(metadata))
+	}
 }
 
 /// The metadata of a runtime.
@@ -50,168 +50,168 @@ impl From<RuntimeMetadataLastVersion> for RuntimeMetadataPrefixed {
 #[derive(PartialEq, Eq, Encode, Debug)]
 #[cfg_attr(feature = "std", derive(Decode))]
 pub struct RuntimeMetadataV13<S: FormString = &'static str> {
-    pub types: PortableRegistry<S>,
-    /// Metadata of all the modules.
-    pub modules: Vec<ModuleMetadata<PortableForm>>,
-    // /// Metadata of the extrinsic.
-    // pub extrinsic: ExtrinsicMetadata<F>,
+	pub types: PortableRegistry<S>,
+	/// Metadata of all the modules.
+	pub modules: Vec<ModuleMetadata<PortableForm>>,
+	// /// Metadata of the extrinsic.
+	// pub extrinsic: ExtrinsicMetadata<F>,
 }
 
 impl RuntimeMetadataV13 {
-    pub fn new(modules: Vec<ModuleMetadata>) -> Self {
-        let mut registry = Registry::new();
-        let modules = registry.map_into_portable(modules);
-        Self {
-            types: registry.into(),
-            modules,
-        }
-    }
+	pub fn new(modules: Vec<ModuleMetadata>) -> Self {
+		let mut registry = Registry::new();
+		let modules = registry.map_into_portable(modules);
+		Self {
+			types: registry.into(),
+			modules,
+		}
+	}
 }
 
 /// Metadata of the extrinsic used by the runtime.
 #[derive(Clone, PartialEq, Eq, Encode, Debug)]
 #[cfg_attr(feature = "std", derive(Decode))]
 pub struct ExtrinsicMetadata<T: Form = MetaForm> {
-    /// Extrinsic version.
-    pub version: u8,
-    /// The signed extensions in the order they appear in the extrinsic.
-    pub signed_extensions: Vec<T::Type>,
+	/// Extrinsic version.
+	pub version: u8,
+	/// The signed extensions in the order they appear in the extrinsic.
+	pub signed_extensions: Vec<T::Type>,
 }
 
 impl IntoPortable for ExtrinsicMetadata {
-    type Output = ExtrinsicMetadata<PortableForm>;
+	type Output = ExtrinsicMetadata<PortableForm>;
 
-    fn into_portable(self, registry: &mut Registry) -> Self::Output {
-        ExtrinsicMetadata {
-            version: self.version,
-            signed_extensions: registry.register_types(self.signed_extensions),
-        }
-    }
+	fn into_portable(self, registry: &mut Registry) -> Self::Output {
+		ExtrinsicMetadata {
+			version: self.version,
+			signed_extensions: registry.register_types(self.signed_extensions),
+		}
+	}
 }
 
 /// All metadata about an runtime module.
 #[derive(Clone, PartialEq, Eq, Encode, Debug)]
 #[cfg_attr(feature = "std", derive(Decode))]
 pub struct ModuleMetadata<T: Form = MetaForm> {
-    pub name: T::String,
-    // pub storage: Option<DecodeDifferent<FnEncode<StorageMetadata>, StorageMetadata>>,
-    pub calls: Option<Vec<FunctionMetadata<T>>>,
-    pub event: Option<Vec<EventMetadata<T>>>,
-    // pub constants: DFnA<ModuleConstantMetadata>,
-    // pub errors: DFnA<ErrorMetadata>,
+	pub name: T::String,
+	// pub storage: Option<DecodeDifferent<FnEncode<StorageMetadata>, StorageMetadata>>,
+	pub calls: Option<Vec<FunctionMetadata<T>>>,
+	pub event: Option<Vec<EventMetadata<T>>>,
+	// pub constants: DFnA<ModuleConstantMetadata>,
+	// pub errors: DFnA<ErrorMetadata>,
 }
 
 impl IntoPortable for ModuleMetadata {
-    type Output = ModuleMetadata<PortableForm>;
+	type Output = ModuleMetadata<PortableForm>;
 
-    fn into_portable(self, registry: &mut Registry) -> Self::Output {
-        ModuleMetadata {
-            name: self.name.into_portable(registry),
-            calls: self.calls.map(|calls| registry.map_into_portable(calls)),
-            event: self.event.map(|event| registry.map_into_portable(event)),
-        }
-    }
+	fn into_portable(self, registry: &mut Registry) -> Self::Output {
+		ModuleMetadata {
+			name: self.name.into_portable(registry),
+			calls: self.calls.map(|calls| registry.map_into_portable(calls)),
+			event: self.event.map(|event| registry.map_into_portable(event)),
+		}
+	}
 }
 
 /// All the metadata about a function.
 #[derive(Clone, PartialEq, Eq, Encode, Debug)]
 #[cfg_attr(feature = "std", derive(Decode))]
 pub struct FunctionMetadata<T: Form = MetaForm> {
-    pub name: T::String,
-    pub arguments: Vec<FunctionArgumentMetadata<T>>,
-    pub documentation: Vec<T::String>,
+	pub name: T::String,
+	pub arguments: Vec<FunctionArgumentMetadata<T>>,
+	pub documentation: Vec<T::String>,
 }
 
 impl IntoPortable for FunctionMetadata {
-    type Output = FunctionMetadata<PortableForm>;
+	type Output = FunctionMetadata<PortableForm>;
 
-    fn into_portable(self, registry: &mut Registry) -> Self::Output {
-        FunctionMetadata {
-            name: self.name.into_portable(registry),
-            arguments: registry.map_into_portable(self.arguments),
-            documentation: registry.map_into_portable(self.documentation),
-        }
-    }
+	fn into_portable(self, registry: &mut Registry) -> Self::Output {
+		FunctionMetadata {
+			name: self.name.into_portable(registry),
+			arguments: registry.map_into_portable(self.arguments),
+			documentation: registry.map_into_portable(self.documentation),
+		}
+	}
 }
 
 /// All the metadata about a function argument.
 #[derive(Clone, PartialEq, Eq, Encode, Debug)]
 #[cfg_attr(feature = "std", derive(Decode))]
 pub struct FunctionArgumentMetadata<T: Form = MetaForm> {
-    pub name: T::String,
-    pub ty: T::Type,
-    pub is_compact: bool,
+	pub name: T::String,
+	pub ty: T::Type,
+	pub is_compact: bool,
 }
 
 impl IntoPortable for FunctionArgumentMetadata {
-    type Output = FunctionArgumentMetadata<PortableForm>;
+	type Output = FunctionArgumentMetadata<PortableForm>;
 
-    fn into_portable(self, registry: &mut Registry) -> Self::Output {
-        FunctionArgumentMetadata {
-            name: self.name.into_portable(registry),
-            ty: registry.register_type(&self.ty),
-            is_compact: self.is_compact,
-        }
-    }
+	fn into_portable(self, registry: &mut Registry) -> Self::Output {
+		FunctionArgumentMetadata {
+			name: self.name.into_portable(registry),
+			ty: registry.register_type(&self.ty),
+			is_compact: self.is_compact,
+		}
+	}
 }
 
 /// All the metadata about an outer event.
 #[derive(Clone, PartialEq, Eq, Encode, Debug)]
 #[cfg_attr(feature = "std", derive(Decode))]
 pub struct OuterEventMetadata<T: Form = MetaForm> {
-    pub name: T::String,
-    pub events: Vec<ModuleEventMetadata<T>>,
+	pub name: T::String,
+	pub events: Vec<ModuleEventMetadata<T>>,
 }
 
 impl IntoPortable for OuterEventMetadata {
-    type Output = OuterEventMetadata<PortableForm>;
+	type Output = OuterEventMetadata<PortableForm>;
 
-    fn into_portable(self, registry: &mut Registry) -> Self::Output {
-        OuterEventMetadata {
-            name: self.name.into_portable(registry),
-            events: registry.map_into_portable(self.events),
-        }
-    }
+	fn into_portable(self, registry: &mut Registry) -> Self::Output {
+		OuterEventMetadata {
+			name: self.name.into_portable(registry),
+			events: registry.map_into_portable(self.events),
+		}
+	}
 }
 
 /// Metadata about a module event.
 #[derive(Clone, PartialEq, Eq, Encode, Debug)]
 #[cfg_attr(feature = "std", derive(Decode))]
 pub struct ModuleEventMetadata<T: Form = MetaForm> {
-    pub name: T::String,
-    pub events: Vec<EventMetadata<T>>,
+	pub name: T::String,
+	pub events: Vec<EventMetadata<T>>,
 }
 
 impl IntoPortable for ModuleEventMetadata {
-    type Output = ModuleEventMetadata<PortableForm>;
+	type Output = ModuleEventMetadata<PortableForm>;
 
-    fn into_portable(self, registry: &mut Registry) -> Self::Output {
-        ModuleEventMetadata {
-            name: self.name.into_portable(registry),
-            events: registry.map_into_portable(self.events),
-        }
-    }
+	fn into_portable(self, registry: &mut Registry) -> Self::Output {
+		ModuleEventMetadata {
+			name: self.name.into_portable(registry),
+			events: registry.map_into_portable(self.events),
+		}
+	}
 }
 
 /// All the metadata about an event.
 #[derive(Clone, PartialEq, Eq, Encode, Debug)]
 #[cfg_attr(feature = "std", derive(Decode))]
 pub struct EventMetadata<T: Form = MetaForm> {
-    pub name: T::String,
-    pub arguments: Vec<TypeSpec<T>>,
-    pub documentation: Vec<T::String>,
+	pub name: T::String,
+	pub arguments: Vec<TypeSpec<T>>,
+	pub documentation: Vec<T::String>,
 }
 
 impl IntoPortable for EventMetadata {
-    type Output = EventMetadata<PortableForm>;
+	type Output = EventMetadata<PortableForm>;
 
-    fn into_portable(self, registry: &mut Registry) -> Self::Output {
-        EventMetadata {
-            name: self.name.into_portable(registry),
-            arguments: registry.map_into_portable(self.arguments),
-            documentation: registry.map_into_portable(self.documentation),
-        }
-    }
+	fn into_portable(self, registry: &mut Registry) -> Self::Output {
+		EventMetadata {
+			name: self.name.into_portable(registry),
+			arguments: registry.map_into_portable(self.arguments),
+			documentation: registry.map_into_portable(self.documentation),
+		}
+	}
 }
 
 /// A type specification.
@@ -234,32 +234,32 @@ impl IntoPortable for EventMetadata {
 #[derive(Clone, PartialEq, Eq, Encode, Debug)]
 #[cfg_attr(feature = "std", derive(Decode))]
 pub struct TypeSpec<T: Form = MetaForm> {
-    /// The actual type.
-    pub ty: T::Type,
-    /// The compile-time known displayed representation of the type.
-    pub name: T::String,
+	/// The actual type.
+	pub ty: T::Type,
+	/// The compile-time known displayed representation of the type.
+	pub name: T::String,
 }
 
 impl IntoPortable for TypeSpec {
-    type Output = TypeSpec<PortableForm>;
+	type Output = TypeSpec<PortableForm>;
 
-    fn into_portable(self, registry: &mut Registry) -> Self::Output {
-        TypeSpec {
-            ty: registry.register_type(&self.ty),
-            name: self.name.into_portable(registry),
-        }
-    }
+	fn into_portable(self, registry: &mut Registry) -> Self::Output {
+		TypeSpec {
+			ty: registry.register_type(&self.ty),
+			name: self.name.into_portable(registry),
+		}
+	}
 }
 
 impl TypeSpec {
-    /// Creates a new type specification without a display name.
-    pub fn new<T>(name: &'static str) -> Self
-        where
-            T: TypeInfo + 'static,
-    {
-        Self {
-            ty: meta_type::<T>(),
-            name,
-        }
-    }
+	/// Creates a new type specification without a display name.
+	pub fn new<T>(name: &'static str) -> Self
+	where
+		T: TypeInfo + 'static,
+	{
+		Self {
+			ty: meta_type::<T>(),
+			name,
+		}
+	}
 }
