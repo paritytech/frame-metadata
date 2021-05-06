b21f2d5d29b37aae29d4a82ccb42111e7ba89d5e
Andrew Jones <ascjones@gmail.com>
2021-05-06 15:18:01 +0100

    Rename StorageMetadata with Pallet prefix for consistency, fix comment.

--- frame-metadata/src/v13.rs
+++ frame-metadata/src/v13.rs
@@ -131,7 +131,7 @@ impl IntoPortable for SignedExtensionMetadata {
 )]
 pub struct PalletMetadata<T: Form = MetaForm> {
 	pub name: T::String,
-	pub storage: Option<StorageMetadata<T>>,
+	pub storage: Option<PalletStorageMetadata<T>>,
 	pub calls: Option<PalletCallMetadata<T>>,
 	pub event: Option<PalletEventMetadata<T>>,
 	pub constants: Vec<PalletConstantMetadata<T>>,
@@ -157,24 +157,24 @@ impl IntoPortable for PalletMetadata {
 	}
 }
 
-/// All metadata of the storage.module
+/// All metadata of the pallet's storage.
 #[derive(Clone, PartialEq, Eq, Encode)]
 #[cfg_attr(feature = "std", derive(Decode, Serialize, Debug))]
 #[cfg_attr(
 	feature = "std",
 	serde(bound(serialize = "T::Type: Serialize, T::String: Serialize"))
 )]
-pub struct StorageMetadata<T: Form = MetaForm> {
+pub struct PalletStorageMetadata<T: Form = MetaForm> {
 	/// The common prefix used by all storage entries.
 	pub prefix: T::String,
 	pub entries: Vec<StorageEntryMetadata<T>>,
 }
 
-impl IntoPortable for StorageMetadata {
-	type Output = StorageMetadata<PortableForm>;
+impl IntoPortable for PalletStorageMetadata {
+	type Output = PalletStorageMetadata<PortableForm>;
 
 	fn into_portable(self, registry: &mut Registry) -> Self::Output {
-		StorageMetadata {
+		PalletStorageMetadata {
 			prefix: self.prefix.into_portable(registry),
 			entries: registry.map_into_portable(self.entries),
 		}
