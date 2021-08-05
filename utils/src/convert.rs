740fe81959322703ba1cd0e5df5a68306d5f3de0
Andrew Jones <ascjones@gmail.com>
2021-08-05 17:47:43 +0100

    WIP Add test for converting v14 to v13

--- utils/src/convert.rs
+++ utils/src/convert.rs
@@ -380,3 +380,30 @@ impl Converter {
 		}
 	}
 }
+
+#[cfg(test)]
+mod tests {
+	use codec::Decode;
+	use frame_metadata::RuntimeMetadataPrefixed;
+	use std::{env, fs, io::Read, path};
+
+	#[test]
+	fn substrate_node_runtime_v14_to_v13() {
+		let root = env::var("CARGO_MANIFEST_DIR").unwrap_or(".".into());
+		let root_path = path::Path::new(&root);
+
+		// generate with:
+		// curl -sX POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"state_getMetadata", "id": 1}' localhost:9933 \
+		// | jq .result \
+		// | cut -d '"' -f 2 \
+		// | xxd -r -p > ./utils/node-runtime-v13.scale
+		//
+		let path = root_path.join("node-runtime-v13.scale");
+
+		let mut file = fs::File::open(path).expect("Error opening metadata file");
+		let mut bytes = Vec::new();
+		file.read_to_end(&mut bytes).expect("Error reading metadata file");
+
+		let metadata = RuntimeMetadataPrefixed::decode(&mut &bytes[..]).expect("Error decoding metadata file");
+	}
+}
